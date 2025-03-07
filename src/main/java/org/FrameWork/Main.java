42552633e0f0cf0dc2089cbab4f03d095412efad
kodevenkatakrishna <venkatakrishna.kode@hcl.com>
2025-03-07 12:45:26 +0530

    First Commit

---
+++ src/main/java/org/FrameWork/Main.java
@@ -0,0 +1,7 @@
+package org.FrameWork;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/SampleClass.java
@@ -0,0 +1,7 @@
+public class SampleClass {
+
+     public static void main(String[] args){
+         System.out.println("Build Successful");
+         System.out.println("Tnq");
+     }
+}
